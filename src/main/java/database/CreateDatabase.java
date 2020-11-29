c869cfbb83532481fc4686fb7f7a1be84a4a2125
Kahzerx <kahzer123@gmail.com>
2020-11-29 19:57:46 +0100

    db

--- src/main/java/database/CreateDatabase.java
+++ src/main/java/database/CreateDatabase.java
@@ -1,4 +1,35 @@
 package database;
 
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class CreateDatabase {
+    private static Connection c = null;
+
+    public static void create(String name) throws SQLException, ClassNotFoundException {
+        boolean createDir = new File("database").mkdir();
+        if (createDir) System.out.println("Carpeta de la base de datos creada");
+        c = DriverManager.getConnection(String.format("jdbc:sqlite:database/%s.db", name));
+        assert c != null;
+        c.setAutoCommit(false);
+        createTables();
+    }
+
+    private static void createTables() throws SQLException {
+        createUserTable();
+    }
+
+    private static void createUserTable() throws SQLException {
+        Statement stmt = c.createStatement();
+
+        String userTable = "CREATE TABLE IF NOT EXISTS `usuarios`(" +
+                "`usuario` TEXT PRIMARY KEY NOT NULL," +
+                "`pass` TEXT NOT NULL);";
+        stmt.executeUpdate(userTable);
+        stmt.close();
+        c.commit();
+    }
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,11 @@
 package main;
 
+import database.CreateDatabase;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        CreateDatabase.create("db");
     }
 }
