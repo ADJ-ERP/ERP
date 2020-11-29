1144c1c3819e2adfa0a03026cf12b1db81c748b4
Kahzerx <kahzer123@gmail.com>
2020-11-29 20:45:07 +0100

    cleanup

--- src/main/java/buttonActions/RegUserAction.java
+++ src/main/java/buttonActions/RegUserAction.java
@@ -1,17 +1,13 @@
 package buttonActions;
 
+import crypt.Pass;
 import database.Query;
 import utils.StringUtils;
 
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.PBEKeySpec;
 import javax.swing.*;
 import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
-import java.security.spec.KeySpec;
 import java.sql.SQLException;
-import java.util.Base64;
 
 public class RegUserAction {
     public static void registerUser(String user, String pass, String verification) throws SQLException, NoSuchAlgorithmException, InvalidKeySpecException {
@@ -20,14 +16,11 @@ public class RegUserAction {
             return;
         }
         if (!StringUtils.rEmpty(user, pass)) {
-            SecureRandom random = new SecureRandom();
-            byte[] salt = new byte[16];
-            random.nextBytes(salt);
-            KeySpec spec = new PBEKeySpec(pass.toCharArray(), salt, 65536, 128);
-            SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-            byte[] hash = f.generateSecret(spec).getEncoded();
-            Base64.Encoder enc = Base64.getEncoder();
-            Query.register(user, enc.encodeToString(hash));
+            String hash = Pass.hashPass(pass);
+            if (hash != null) {
+                Query.register(user, hash);
+            }
+            else JOptionPane.showMessageDialog(null, "Error al registrar usuario", "ERROR", JOptionPane.ERROR_MESSAGE);
         }
         else JOptionPane.showMessageDialog(null, "Debes completar todos los campos", "ERROR", JOptionPane.ERROR_MESSAGE);
     }
