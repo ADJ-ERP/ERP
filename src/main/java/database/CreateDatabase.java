b9cc1da22cc67aa66b4c9e07fac150385893e082
Kahzerx <kahzer123@gmail.com>
2020-12-26 15:18:35 +0100

    bit of refactor

--- src/main/java/database/CreateDatabase.java
+++ src/main/java/database/CreateDatabase.java
@@ -1,5 +1,5 @@
 /*
-Esta clase es para manejar la creacion de la Base de Datos 
+Esta clase es para manejar la creación de la Base de Datos.
 */
 
 package database;
@@ -14,19 +14,19 @@ public class CreateDatabase {
     public static Connection c = null;
 
     public static void create(String name) throws SQLException {
-        boolean createDir = new File("database").mkdir();// creamos la carpeta donde estara la Base de Datos
+        boolean createDir = new File("database").mkdir();  // Creamos la carpeta donde estará la Base de Datos.
         if (createDir) System.out.println("Carpeta de la base de datos creada");
-        c = DriverManager.getConnection(String.format("jdbc:sqlite:database/%s.db", name));// creamos la Base de Datos
+        c = DriverManager.getConnection(String.format("jdbc:sqlite:database/%s.db", name));  // Creamos la Base de Datos.
         assert c != null;
         c.setAutoCommit(false);
         createTables();
     }
 
-    private static void createTables() throws SQLException {// estoa albergara toda la creacion de tablas
+    private static void createTables() throws SQLException {  // Esto contiene toda la creación de tablas.
         createUserTable();
     }
 
-    private static void createUserTable() throws SQLException {// creamos la tabla Usuarios
+    private static void createUserTable() throws SQLException {  // Creamos la tabla Usuarios.
         Statement stmt = c.createStatement();
 
         String userTable = "CREATE TABLE IF NOT EXISTS `usuarios`(" +
