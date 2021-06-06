4076e741c7e413f7f73a722866cda708ed74a5c4
Kahzerx <kahzer123@gmail.com>
2021-06-06 16:58:12 +0200

    final

--- src/main/java/database/tables/ClientDB.java
+++ src/main/java/database/tables/ClientDB.java
@@ -1,11 +1,11 @@
 package database.tables;
 
 public class ClientDB {
-    private String nombre;
-    private String CIF;
-    private String telefono;
-    private String correo;
-    private String direccion;
+    private final String nombre;
+    private final String CIF;
+    private final String telefono;
+    private final String correo;
+    private final String direccion;
 
     public ClientDB(String nombre, String CIF, String telefono, String correo, String direccion) {
         this.nombre = nombre;
--- src/main/java/database/tables/PartidaDB.java
+++ src/main/java/database/tables/PartidaDB.java
@@ -1,24 +1,24 @@
 package database.tables;
 
 public class PartidaDB {
-    private int numPartida;
-    private String fechaAlta;
-    private String tipo;
-    private String centroVenta;
-    private int numMatadero;
-    private String proveedor;
-    private int numExplotacion;
-    private String paisNacido;
-    private String paisSacrificado;
-    private String tipoAnimal;
-    private int totalAnimales;
-    private int delNum;
-    private int alNum;
-    private int totalKgBrutos;
-    private int porcenOreo;
-    private int totalKgNetos;
-    private int importeTotalCosto;
-    private String notas;
+    private final int numPartida;
+    private final String fechaAlta;
+    private final String tipo;
+    private final String centroVenta;
+    private final int numMatadero;
+    private final String proveedor;
+    private final int numExplotacion;
+    private final String paisNacido;
+    private final String paisSacrificado;
+    private final String tipoAnimal;
+    private final int totalAnimales;
+    private final int delNum;
+    private final int alNum;
+    private final int totalKgBrutos;
+    private final int porcenOreo;
+    private final int totalKgNetos;
+    private final int importeTotalCosto;
+    private final String notas;
 
     public PartidaDB(int numPartida, String fechaAlta, String tipo, String centroVenta, int numMatadero, String proveedor, int numExplotacion, String paisNacido, String paisSacrificado, String tipoAnimal, int totalAnimales, int delNum, int alNum, int totalKgBrutos, int porcenOreo, int totalKgNetos, int importeTotalCosto, String notas) {
         this.numPartida = numPartida;
@@ -45,144 +45,72 @@ public class PartidaDB {
         return numPartida;
     }
 
-    public void setNumPartida(int numPartida) {
-        this.numPartida = numPartida;
-    }
-
     public String getFechaAlta() {
         return fechaAlta;
-    }
-
-    public void setFechaAlta(String fechaAlta) {
-        this.fechaAlta = fechaAlta;
     }
 
     public String getTipo() {
         return tipo;
     }
 
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
     public String getCentroVenta() {
         return centroVenta;
-    }
-
-    public void setCentroVenta(String centroVenta) {
-        this.centroVenta = centroVenta;
     }
 
     public int getNumMatadero() {
         return numMatadero;
     }
 
-    public void setNumMatadero(int numMatadero) {
-        this.numMatadero = numMatadero;
-    }
-
     public String getProveedor() {
         return proveedor;
-    }
-
-    public void setProveedor(String proveedor) {
-        this.proveedor = proveedor;
     }
 
     public int getNumExplotacion() {
         return numExplotacion;
     }
 
-    public void setNumExplotacion(int numExplotacion) {
-        this.numExplotacion = numExplotacion;
-    }
-
     public String getPaisNacido() {
         return paisNacido;
-    }
-
-    public void setPaisNacido(String paisNacido) {
-        this.paisNacido = paisNacido;
     }
 
     public String getPaisSacrificado() {
         return paisSacrificado;
     }
 
-    public void setPaisSacrificado(String paisSacrificado) {
-        this.paisSacrificado = paisSacrificado;
-    }
-
     public String getTipoAnimal() {
         return tipoAnimal;
-    }
-
-    public void setTipoAnimal(String tipoAnimal) {
-        this.tipoAnimal = tipoAnimal;
     }
 
     public int getTotalAnimales() {
         return totalAnimales;
     }
 
-    public void setTotalAnimales(int totalAnimales) {
-        this.totalAnimales = totalAnimales;
-    }
-
     public int getDelNum() {
         return delNum;
-    }
-
-    public void setDelNum(int delNum) {
-        this.delNum = delNum;
     }
 
     public int getAlNum() {
         return alNum;
     }
 
-    public void setAlNum(int alNum) {
-        this.alNum = alNum;
-    }
-
     public int getTotalKgBrutos() {
         return totalKgBrutos;
-    }
-
-    public void setTotalKgBrutos(int totalKgBrutos) {
-        this.totalKgBrutos = totalKgBrutos;
     }
 
     public int getPorcenOreo() {
         return porcenOreo;
     }
 
-    public void setPorcenOreo(int porcenOreo) {
-        this.porcenOreo = porcenOreo;
-    }
-
     public int getTotalKgNetos() {
         return totalKgNetos;
-    }
-
-    public void setTotalKgNetos(int totalKgNetos) {
-        this.totalKgNetos = totalKgNetos;
     }
 
     public int getImporteTotalCosto() {
         return importeTotalCosto;
     }
 
-    public void setImporteTotalCosto(int importeTotalCosto) {
-        this.importeTotalCosto = importeTotalCosto;
-    }
-
     public String getNotas() {
         return notas;
-    }
-
-    public void setNotas(String notas) {
-        this.notas = notas;
     }
 
     @Override
